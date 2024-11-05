d9aa02a3740b27654cdcfc4605197407d458d686
Uzair Khan <123413954+Uzee62@users.noreply.github.com>
2024-11-05 23:51:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from pyod.models.knn import KNN
 import streamlit as st
 
 def train_model():
-    """Training the model using PYOD's KNN and label outliers using knn.labels_."""
+    #Training the model using PYOD's KNN and label outliers using knn.labels_.
     if st.session_state['processed_data'] is not None:
         knn = KNN()
         knn.fit(st.session_state['processed_data'])
